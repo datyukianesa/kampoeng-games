6c3660ba5e11dc01fe4745e6aada15d156547a0a
Abyanzhafran <zhafranjupleks5758@gmail.com>
2021-06-21 08:34:16 +0700

    feat(page): :sparkles: add login n contentDashboard page

---
+++ src/pages/contentDashboard.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function contentDashboard() {
+  return (
+    <div>
+      <h4 className="text-dark text-xl">
+        dashboard admin tersidia fitur blablablablabla.....!!!
+      </h4>
+    </div>
+  )
+}
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,27 +1,43 @@
 import React from "react"
+import Card from "@material-tailwind/react/Card"
+import CardHeader from "@material-tailwind/react/CardHeader"
+import CardBody from "@material-tailwind/react/CardBody"
 import Button from "@material-tailwind/react/Button"
-// import Sidebar from "../components/Sidebar"
-// import LayoutAdmin from "../components/LayoutAdmin"
+import Input from "@material-tailwind/react/Input"
 import "@material-tailwind/react/tailwind.css"
 
 export default function login() {
   return (
-    <div>
-      {/* <Sidebar /> */}
-      {/* <LayoutAdmin /> */}
-      <div class="ml-96">
-        <h1 class="text-4xl text-bolder">Halaman Login niii...!!</h1>
-        <Button
-          color="lightBlue"
-          buttonType="filled"
-          rounded={false}
-          block={false}
-          iconOnly={false}
-          ripple="light"
-        >
-          Button
-        </Button>
-      </div>
+    <div className="m-8">
+      <Card>
+        <CardHeader color="amber" contentPosition="none">
+          <div className="w-full flex items-center justify-between">
+            <h2 className="text-white text-2xl">Login</h2>
+          </div>
+        </CardHeader>
+        <CardBody>
+          <form>
+            <div className="flex flex-wrap mt-10">
+              <div className="w-full lg:w-6/12 pr-4 mb-10 font-light">
+                <Input type="text" color="purple" placeholder="Username" />
+              </div>
+              <div className="w-full lg:w-6/12 pr-4 mb-10 font-light">
+                <Input type="text" color="purple" placeholder="Password" />
+              </div>
+            </div>
+            <Button
+              color="lightBlue"
+              buttonType="filled"
+              size="regular"
+              rounded={false}
+              block={false}
+              ripple="light"
+            >
+              Login
+            </Button>
+          </form>
+        </CardBody>
+      </Card>
     </div>
   )
 }
