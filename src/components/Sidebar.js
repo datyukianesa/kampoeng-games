718818b7675bb9a17483a02092e95371062ac867
Abyanzhafran <zhafranjupleks5758@gmail.com>
2021-06-24 13:41:10 +0700

    fix(ui): :fire: sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -13,7 +13,7 @@ const Sidebar = () => {
     <div>
       <NavbarAdmin showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
       <div
-        class={`h-screen fixed top-0 md:left-0 ${showSidebar} overflow-y-auto flex-row flex-nowrap overflow-hidden shadow-xl bg-secondary w-64 z-10 py-2 px-6 transition-all duration-300`}
+        class={`h-screen fixed top-0 md:left-0 ${showSidebar} overflow-y-auto flex-row flex-nowrap overflow-hidden shadow-xl bg-secondary w-64 z-10 px-6 transition-all duration-300`}
       >
         <div className="flex-col items-stretch min-h-full flex-nowrap px-0 relative">
           <div class="flex flex-row items-center pt-2">
@@ -30,12 +30,12 @@ const Sidebar = () => {
             <h6 class="text-white text-md font-bold">Dashboard Admin</h6>
           </div>
           <div className="flex flex-col">
-            <hr className="my-2 min-w-full" />
+            <hr className="min-w-full" />
+            <div className="w-16 rounded-full bg-yellow-400 mx-auto my-4">
+              <LogoIcon className={"p-2"} />
+            </div>
 
             <ul className="flex flex-col min-w-full list-none grid-cols gap-6">
-              <div className="w-20 h-20 rounded-full bg-yellow-400 mx-auto my-2">
-                <LogoIcon className={"p-2"} />
-              </div>
               <Link to="/login">
                 <Button
                   class="mb-4 w-full"
