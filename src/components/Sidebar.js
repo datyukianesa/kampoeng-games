97a67df0d74117561cfe51e3227374d4f90e7daa
Abyanzhafran <zhafranjupleks5758@gmail.com>
2021-06-24 14:11:03 +0700

    feat(ui): :art: change button function sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -24,6 +24,7 @@ const Sidebar = () => {
               iconOnly
               rounded
               ripple="light"
+              onClick={() => setShowSidebar("-left-64")}
             >
               <span class="material-icons md-28 text-white">menu</span>
             </Button>
