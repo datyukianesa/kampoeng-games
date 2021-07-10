35800e010e069bb824ebdfd331556f9bad0b93b2
Abyanzhafran <zhafranjupleks5758@gmail.com>
2021-07-10 10:21:02 +0700

    feat: update express-config

--- express-config.js
+++ express-config.js
@@ -7,7 +7,12 @@ var port = process.env.PORT || 3005
 
 // Connect to our database
 database.connect(err => {
-  if (err) throw err
+  if (!err) {
+    console.log("Db connect success")
+  }
+  if (err) {
+    console.log("Db losttt")
+  }
 })
 
 // This is to allow our api for cross-origin resource sharing
@@ -23,30 +28,30 @@ app.use(
   })
 )
 
-app.get("/tweets", res => {
-  let sql = `SELECT * FROM tweet`
+app.get("/tweets", (req, res) => {
+  let sql = `SELECT * FROM users`
 
   database.query(sql, (err, result) => {
+    if (!err) {
+      res.send(result)
+    }
     if (err) {
       res.status(400).send(err)
       return
     }
-
-    if (result.lenght) res.json(result)
-    else res.json({})
   })
 })
 
-// Register routes in the main index.js
-// app.use("/", [require("./routes/tweet"), require("./routes/auth")])
+app.get("/", (req, res) => {
+  res.send("Hey World...i will travel you")
+})
 
-// http://localhost:3005/tweets - GET, POST
-// http://localhost:3005/tweets/user/:id - GET
-// http://localhost:3005/tweets/:id - DELETE
-// http://localhost:3005/authenticate - POST for login session
+app.get("/about", (req, res) => {
+  res.send("about")
+})
 
-app.get("/", (request, response) => {
-  response.send("Hey World...i lov youuuu")
+app.get("/show", (req, res) => {
+  res.send("show")
 })
 
 app.listen(port, () => {
