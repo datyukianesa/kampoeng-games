76b151d153382b1c93df1875ace9915398265c83
Abyanzhafran <zhafranjupleks5758@gmail.com>
2021-07-15 06:33:49 +0700

    feat: swap page content

--- src/pages/dashboardSet.js
+++ src/pages/dashboardSet.js
@@ -1,48 +1,22 @@
 import React from "react"
 import Card from "@material-tailwind/react/Card"
-import CardHeader from "@material-tailwind/react/CardHeader"
 import CardBody from "@material-tailwind/react/CardBody"
+import Heading from "../components/Heading"
+import Paragraph from "../components/Paragraph"
+import ProfileImage from "../components/ProfileImage"
+import { StaticImage } from "gatsby-plugin-image"
 import Button from "@material-tailwind/react/Button"
+import LogoIcon from "../svg/LogoIcon"
 import LayoutDashboard from "../components/layoutDashboard"
 import "@material-tailwind/react/tailwind.css"
 
 export default function dashboardSet() {
-  const tableItem = [
+  const cardItem = [
     {
-      id: "1",
       name: "Khalif Farrel Yuandra",
       title: "Front-end Designer",
       nim: "19.11.3256",
-    },
-    {
-      id: "2",
-      name: "Ignatius Tegar Saputra",
-      title: "Front-end Designer",
-      nim: "19.11.3298",
-    },
-    {
-      id: "3",
-      name: "Bilal Muhtasyimbillah",
-      title: "Front-end Designer",
-      nim: "19.11.3272",
-    },
-    {
-      id: "4",
-      name: "Fanni Fadhila",
-      title: "Back-end Developer",
-      nim: "19.11.3300",
-    },
-    {
-      id: "5",
-      name: "Abyan Zhafran Daffa",
-      title: "Back-end Developer",
-      nim: "19.11.3252",
-    },
-    {
-      id: "6",
-      name: "Nadhira Triadha Pitaloka",
-      title: "Front-end Designer",
-      nim: "19.11.3270",
+      imgUrl: "../images/farrel.png",
     },
   ]
 
@@ -50,70 +24,55 @@ export default function dashboardSet() {
     <LayoutDashboard>
       <div className="h-screen flex items-center justify-center">
         <Card className="w-full border border-gray-300 mx-6">
-          <CardHeader color="blue" contentPosition="none">
-            <div className="w-full flex items-center justify-between">
-              <h2 className="text-white text-2xl">Page Visits</h2>
-              <Button
-                color="transparent"
-                buttonType="link"
-                size="lg"
-                style={{ padding: 0 }}
-              >
-                See More
-              </Button>
+          <div className="flex flex-row flex-grow gap-4">
+            <div className="w-12 h-12 md:w-16 rounded-full bg-yellow-400 border-2 border-gray-900">
+              <LogoIcon className={"p-1 md:p-2"} />
             </div>
-          </CardHeader>
+            <span className="text-dark font-bold text-xl flex items-center">
+              dashboard
+            </span>
+          </div>
           <CardBody>
-            <div className="overflow-x-auto">
-              <table className="items-center w-full bg-transparent border-collapse">
-                <thead>
-                  <tr>
-                    <th className="px-2 text-teal-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
-                      ID
-                    </th>
-                    <th className="px-2 text-teal-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
-                      Name
-                    </th>
-                    <th className="px-2 text-teal-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
-                      Position
-                    </th>
-                    <th className="px-2 text-teal-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
-                      NIM
-                    </th>
-                    <th className="px-2 text-teal-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-light text-left">
-                      Action
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {tableItem.map(item => {
-                    return (
-                      <tr>
-                        <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                          {item.id}
-                        </th>
-                        <td className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                          {item.name}
-                        </td>
-                        <td className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                          {item.title}
-                        </td>
-                        <td className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                          {item.nim}
-                        </td>
-                        <td className="border-b border-gray-200 align-middle font-semibold text-sm whitespace-nowrap px-2 py-4 text-left text-bold">
-                          <span className="text-blue-600 cursor-pointer pr-4">
-                            Edit
-                          </span>
-                          <span className="text-red-400 cursor-pointer">
-                            Delete
-                          </span>
-                        </td>
-                      </tr>
-                    )
-                  })}
-                </tbody>
-              </table>
+            <div className={`grid grid-cols-1 md:grid-cols-3 md:m-2`}>
+              {cardItem.map(item => {
+                return (
+                  <Card className={`text-center flex-1 md:mx-2`}>
+                    <ProfileImage>
+                      <StaticImage
+                        className="w-20 h-20 rounded-full m-5"
+                        loading="eager"
+                        src="../images/farrel.png"
+                        alt="Khalif Farrel"
+                      ></StaticImage>
+                    </ProfileImage>
+                    <Heading className={`text-complementary`}>
+                      {item.name}
+                    </Heading>
+                    <div className="py-2">
+                      <Paragraph className={`text-complementary-darker`}>
+                        {item.title}
+                      </Paragraph>
+                      <Paragraph className={`text-complementary-darker`}>
+                        {item.nim}
+                      </Paragraph>
+                    </div>
+                  </Card>
+                )
+              })}
+              <Card className={`flex items-center justify-center md:mx-2`}>
+                {/* <div class="flex items-center"> */}
+                <Button
+                  color="transparent"
+                  buttonType="filled"
+                  size="lg"
+                  iconOnly
+                  rounded
+                  ripple="light"
+                >
+                  <span class="material-icons md-48 text-yellow-400">add</span>
+                </Button>
+                {/* </div> */}
+              </Card>
             </div>
           </CardBody>
         </Card>
